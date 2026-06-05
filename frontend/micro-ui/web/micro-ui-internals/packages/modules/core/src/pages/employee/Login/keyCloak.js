6c7e3e351721ec235e533235a6d8460161002f10
arshsrivastava21-dot <arsh_srivastava@sparrowsoftech.net>
2026-06-05 16:28:38 +0530

    feat: enhance Keycloak redirection logic for authenticated users and update app routing

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/Login/keyCloak.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/Login/keyCloak.js
@@ -5,6 +5,17 @@ let _kc;
 export const initKeycloak = async () => {
   if (_kc) return _kc;
 
+  const path = window.location.pathname;
+  if (
+    !path.includes("/digit-ui/citizen/home") &&
+    !path.includes("/digit-ui/home") &&
+    !path.includes("/digit-ui/employee/user/login") &&
+    !path.includes("/digit-ui/citizen/login") &&
+    (path.includes("/digit-ui/employee") || path.includes("/digit-ui/citizen"))
+  ) {
+    sessionStorage.setItem("post_keycloak_redirect", window.location.pathname + window.location.search);
+  }
+
   _kc = new Keycloak({
     url: "https://dev-djb.nitcon.in/keycloak",
     realm: "DL",
